11c46030e6099f36de6f812cb721f559a81272a1
inakiarostegui <inakiarostegui98@gmail.com>
2021-05-31 00:24:34 +0300

    allocator interface fix

--- IOI Mentoring/IAllocator.h
+++ IOI Mentoring/IAllocator.h
@@ -15,6 +15,8 @@ public:
 
 	virtual void Clear() = 0;
 
+	virtual void Reset() = 0;
+
 	// Prints information about the allocator, for debugging purposes
 	virtual void PrintData(const bool print_contents) const = 0;
 };
